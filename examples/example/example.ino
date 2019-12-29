98c9d8dad451b23a0fec2a1b9e351f101e503960
Cubiwan <cubiwan@gmail.com>
2019-12-30 00:14:48 +0100

    Update example.ino

--- examples/example/example.ino
+++ examples/example/example.ino
@@ -1,10 +1,13 @@
 #include <efsm.h>
 
+int buttonPin = 7;
+
 STATES {start, step1, step2, finish};
-EVENTS {next, back};
+EVENTS {next, back, restart};
 INIT(start)
 
 void show(){
+  Serial.println();
   if(isState(start)){
     Serial.println("X - - -");
   } else if(isState(step1)){
@@ -13,7 +16,7 @@ void show(){
     Serial.println("- - X -");
   } else if(isState(finish)){ 
     Serial.println("- - - X");  
-  }
+  }  
 }
 
 START_TRANSITIONS
@@ -22,46 +25,52 @@ TRANSITION(next,start,step1,Serial.println("next")) //next start -> step1
 TRANSITION(next,step1,step2,Serial.println("next")) //next start -> step1
 TRANSITION(next,step2,finish,Serial.println("next")) //next start -> step1
 TRANSITION(back,step2,step1,Serial.println("back")) //back step2 -> step1
+TRANSITION(restart,ANY_STATE,start,Serial.println("restart")) //restart any state -> start
 TRANSITION(ANY_EVENT,ANY_STATE,ANY_STATE,Serial.println("FAIL!!!")) //in any other case
 END_TRANSITIONS
 
 START_EXECUTIONS
 Serial.print("state: ");
-EXECUTION(start,Serial.println("start");show()) //state is start
-EXECUTION(step1,Serial.println("step1");show()) //state is step1
-EXECUTION(step2,Serial.println("step2");show()) //state is step2
-EXECUTION(finish,Serial.println("finish");show()) //state is finish
+EXECUTION(start,Serial.println("start")) //state is start
+EXECUTION(step1,Serial.println("step1")) //state is step1
+EXECUTION(step2,Serial.println("step2")) //state is step2
+EXECUTION(finish,Serial.println("finish")) //state is finish
 END_EXECUTIONS
 
-START_COUNTERS
-COUNTER(step1,10,next);
-END_COUNTERS
+START_TRIGGERS
+Serial.println("Triggers");
+COUNTER(start,5,next); //State start wait 5 iterations and launch event next
+TIMER(step1,2000,next); //State step1 wait 2 sg and launch event next
+CONDITIONAL(start, digitalRead(buttonPin) == HIGH, restart); //State finish wait until push button an launch event restart
+END_TRIGGERS
 
 void setup() {
-  Serial.begin(9600); 
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT); 
+  resetTimer();
+  resetCounter();  
 }
 
-int transitionNumber = 0;
-int executionNumber = 0;
-int stepLoop = 0;
+int backFlag = 0;
 
 void loop() {  
-  executionNumber = efsmExecute();
+  show();
 
-  if(isState(step2) && stepLoop < 5){
-    transitionNumber = efsmEvent(back);  
-  } else {
-    transitionNumber = efsmEvent(next);
-  }
-
-  Serial.print("Transition: ");
-  Serial.print(transitionNumber);
-  Serial.print(transitionNumber);
-  Serial.print(transitionNumber);
-
-  if(isState(finish)){    
+  efsmExecute();
+  
+  if(isState(finish)){
+    backFlag = 0;    
     changeState(start);
   }
 
-  delay(2000);
+  if(isState(step2) && backFlag == 0){
+    backFlag = 1;
+    efsmEvent(back);  
+  } else if(isState(step2) && backFlag == 1){
+    efsmEvent(next);
+  }
+  
+  efsmTriggers();
+  
+  delay(1000);
 }
